dba2ec3c7482c599896f1c0cbd2749174cc5d79e
MikkoM <muhonen.mikko@gmail.com>
2021-04-08 00:01:43 +0300

    mvc loppu

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 const express = require('express')
 const path = require('path')
-// const bodyparser = require('body-parser')
-// const expressHbs = require('express-handlebars')
 
 const app = express()
 
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: __dirname + '/views/layouts',
-//   })
-// )
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-const adminData = require('./routes/admin')
+const errorController = require('./controllers/error')
+
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-  //   res.status(404).send('<h1>Page not found</h1>')
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', { pageTitle: 'Page not found--' })
-})
+app.use(errorController.get404)
 
 app.listen(3000, () => console.log('Listening'))
--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,12 @@
 const express = require('express')
 const path = require('path')
 
-const rootDir = require('../util/path')
+const productsController = require('../controllers/products')
 
 const router = express.Router()
 
-const products = []
+router.get('/add-product', productsController.getAddProduct)
 
-router.get('/add-product', (req, res, next) => {
-  //   console.log('add product')
-  //   res.send(
-  //     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  //   )
-  //   res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-  res.render('add-product', {
-    pageTitle: 'Add product',
-    path: '/admin/add-product',
-    formCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  })
-})
+router.post('/add-product', productsController.postAddProduct)
 
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
-  products.push({ title: req.body.title })
-  res.redirect('/')
-})
-
-exports.routes = router
-exports.products = products
+module.exports = router
