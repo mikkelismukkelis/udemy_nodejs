8501bee568e07066f31f3b83b1ee13f231d15ba2
MikkoM <muhonen.mikko@gmail.com>
2021-04-11 21:29:39 +0300

    dynamic routin etc

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const path = require('path')
+const path = require('path');
 
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+const errorController = require('./controllers/error');
 
-const errorController = require('./controllers/error')
+const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(errorController.get404)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(3000, () => console.log('Listening'))
+app.use(errorController.get404);
+
+app.listen(3000);
